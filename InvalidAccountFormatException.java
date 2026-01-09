6b1ac2f74163da2ae46f9734555db992a02f654c
Johnson C. Ang <jcang1@headoffice.corp.bpi.com.ph>
2026-01-09 14:20:35 +0800

    initial Add Java files for M4_Activity5

---
+++ InvalidAccountFormatException.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package m4.activity5;
+
+/**
+ * 
+ */
+public class InvalidAccountFormatException extends RuntimeException {
+	public InvalidAccountFormatException (String message) {
+		super(message);
+		}
+}
---
+++ InvalidAccountNumberException.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package m4.activity5;
+
+/**
+ * 
+ */
+public class InvalidAccountNumberException extends Exception {
+	public InvalidAccountNumberException (String message) {
+		super(message);
+		}
+}
