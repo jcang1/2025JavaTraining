789521ffb59c79a9e0debd0eac32ade54efe49a9
Johnson C. Ang <jcang1@headoffice.corp.bpi.com.ph>
2026-02-25 15:52:50 +0800

    initial submit for M8_Day2_Exercise5

---
+++ src/main/java/com/example/demo/service/BookService.java
@@ -0,0 +1,22 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.model.Book;
+
+public interface BookService {
+	
+	List<Book> getAllBooks();
+
+	Book getBookByID(long id) throws Exception;
+	
+	Book addBook(Book book) throws Exception;
+	
+	List<Book> searchByTitle(String title);
+	
+	List<Book> searchByAuthor(String author);
+	
+	List<Book> searchByAvailability(Boolean availability);
+	
+	List<Book> searchByAuthorAndAvailability(String author, Boolean availability);
+}
